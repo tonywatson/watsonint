479194c6890ab72a52fff096e9ee5085b2a8a32c
Tony Watson <tony@watsonint.com>
2012-08-05 13:03:11 -0400

    memoized project name and added spacing between thumbnails

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
-class Project < ActiveRecord::Base
+class Project < ActiveRecord::Base.extend(ActiveSupport::Memoizable)
   attr_accessible :caption, :description, :name, :skills, :url
   
   has_many :images, :as => :imageable
   
   serialize :skills
+
+  def project_name
+    name
+  end
+  memoize :project_name
   
 end
