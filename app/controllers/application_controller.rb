20ad097325b9612b42093a85b2b19d5917c87629
Tony Watson <tony@watsonint.com>
2012-08-05 16:01:26 -0400

    added 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   private
+  
     def current_user
       # return @current_user if defined?(@current_user)
       # @current_user = current_user_session && current_user_session.user
